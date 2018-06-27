9ccd82d0fa878598ab56bdffee63f64fc3cecd55
Tomasz Giszczak <Tomasz.Giszczak@tandem.co.uk>
2018-06-27 15:30:15 +0100

    First commit

---
+++ app/src/main/java/uk/co/tomek/judopayapp/MainActivity.kt
@@ -0,0 +1,54 @@
+package uk.co.tomek.judopayapp
+
+import android.content.Intent
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import com.judopay.Judo
+import com.judopay.PaymentFragment
+import com.judopay.model.Currency
+import java.util.UUID.randomUUID
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_main)
+
+        val bundle = Bundle()
+        bundle.putParcelable(Judo.JUDO_OPTIONS, Judo.Builder()
+                .setApiToken(API_TOKEN)
+                .setApiSecret(API_SECRET)
+                .setJudoId(JUDO_ID)
+                .setEnvironment(Judo.SANDBOX)
+                .setConsumerReference(randomUUID().toString())
+                .setAmount("0.05")
+                .setCurrency(Currency.GBP)
+                .build())
+
+        val fragment = PaymentFragment()
+        fragment.arguments = bundle
+
+        fragmentManager
+                .beginTransaction()
+                .add(android.R.id.content, fragment)
+                .commit()
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == Judo.JUDO_REQUEST) {
+            when (resultCode) {
+                Judo.RESULT_SUCCESS -> Log.d("Judopay", "Payment successful")
+                Judo.RESULT_ERROR -> Log.e("Judopay", "Error during payment")
+            }
+        }
+    }
+
+    companion object {
+        private const val JUDO_ID = "xxx"
+        private const val API_TOKEN = "xxx"
+        private const val API_SECRET = "xxx"
+    }
+}
